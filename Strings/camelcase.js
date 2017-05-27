3c22f28047e0b4d69b38176d5aac56e5632cb1c6
toddsby <toddsby@gmail.com>
2017-05-27 16:50:46 -0500

    added simple test

--- Strings/camelcase.js
+++ Strings/camelcase.js
@@ -11,12 +11,26 @@ speak('i\'m alive!');
 // attach cameCase method to String prototype
 String.prototype.camelCase = function () {
   return this.replace(/\w+/g, function(a){ 
+    console.log('match: ', a);
     return a.charAt(0).toUpperCase() + a.slice(1).toLowerCase()
   })
 }
 
 // get element by id
 var el = document.getElementById('testcase');
+// get simple element by id
+var simple = document.getElementById('simple').textContent;
 
+console.log('before: ', el.textContent);
 // camelCase content of element
-console.log(el.textContent.camelCase());
+console.log('after: ', el.textContent.camelCase());
+
+console.log(simple);
+
+function removeSpace(str) {
+  return str.replace(/\s+/g, function(match, offset, original){ 
+    return original.substr(0, offset);
+  })
+}
+
+console.log('slice: ', removeSpace(simple));
