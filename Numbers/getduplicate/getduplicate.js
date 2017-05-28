2dce50cc7388f854d08410851e77fdf3cfe85ed4
toddsby <toddsby@gmail.com>
2017-05-28 16:25:50 -0500

    added docs to getduplicate

--- Numbers/getduplicate/getduplicate.js
+++ Numbers/getduplicate/getduplicate.js
@@ -10,11 +10,33 @@ speak('i\'m alive!');
 
 function getDuplicate(items) {
   console.log('og', items.split(' '));
+  // cast string to array, break on white space
+  // accumlate an array, compare position of indexOf against current index, 
+  // if accumulators indexOf is -1 (ie missing) then add current element to accumulator 
   return items.split(' ').reduce(function(acc, el, i, arr) {
     if (arr.indexOf(el) !== i && acc.indexOf(el) < 0) acc.push(el); 
     return acc;
   }, []);
 };
+
+// alternative codementor 
+/*
+
+  function findDupes(items) {
+    var dupes = [],
+    obj = {};
+    
+    for(var i = 0; i < items.length; i++) {
+      if( obj[items[i]] ) {
+        obj[items[i]] = 1;
+      } else {
+        dupes.push(items[i]);
+      }
+    }
+    return dupes;
+  };
+
+ */
 
 // alternative stack overflow 
 // https://stackoverflow.com/a/24968449/3150543
@@ -32,6 +54,32 @@ const duplicates = dict =>
 
 */
 
+// second alternative using forEach
+// http://js-algorithms.tutorialhorizon.com/2016/01/25/find-duplicates-in-an-array/
+/*
+function findDuplicates(data) {
+
+  let result = [];
+
+  data.forEach(function(element, index) {
+    
+    // Find if there is a duplicate or not
+    if (data.indexOf(element, index + 1) > -1) {
+      
+      // Find if the element is already in the result array or not
+      if (result.indexOf(element) === -1) {
+        result.push(element);
+      }
+    }
+  });
+
+  return result;
+}
+
+console.log( findDuplicates([]) ); // []
+console.log( findDuplicates([1, 1, 1]) ); // [1]
+console.log( findDuplicates([1, 2, 3, 1, 2, 1]) ); // [1, 2]
+*/
 
 // get element by id
 var test = document.getElementById('testcase').textContent;
